ac36d310bb10504a703d3e824875aa923bc2f841
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-08 16:53:07 +0300

    fix mobile main padding top

--- src/components/Main/Main.styled.js
+++ src/components/Main/Main.styled.js
@@ -1,3 +1,5 @@
+import { device } from 'utils/mediaQueries';
+
 const { default: styled } = require('@emotion/styled');
 
 export const MainStyle = styled.main`
@@ -12,4 +14,7 @@ export const MainStyle = styled.main`
     radial-gradient(circle, ${props => (props.theme.main.backgroundDots)} 3px, transparent 3px), 
     radial-gradient(circle, ${props => (props.theme.main.backgroundDots)} 3px, transparent 3px);
 
+  ${device.mobile} {
+    padding-top: 120px;
+  }
 `;
