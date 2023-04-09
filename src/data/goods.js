04ccce7faa64073eb9d964356a3f7af13a98069e
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-09 13:06:27 +0300

    add more goods data

--- src/data/goods.js
+++ src/data/goods.js
@@ -67,12 +67,13 @@ export const goods = [
     },
 
     {
-        name: "Боді з довгим рукавом",
+        name: "Курточка демісезонна",
         id: "id-7",
-        price: 130,
-        photo: [],
+        price: 865,
+        photo: [
+            {src:"https://i.ibb.co/HFsDV2D/photo-2023-04-09-12-27-17.jpg", alt:""},],
         category: [],
-        description: "Колір: рожевий синій, зелений. Розмір 86."
+        description: "розмір 128"
     },
 
     {
@@ -103,7 +104,7 @@ export const goods = [
         id: "id-10",
         price: 510,
         photo: [
-            {src:"https://i.ibb.co/Hq1myG9/photo-2023-03-15-19-48-42.jpg", alt:"ромпер"}
+            {src:"https://i.ibb.co/NT5kvGJ/photo-2023-04-09-12-31-41.jpg", alt:"ромпер"}
         ],
         category: ["одяг","для найменших"],
         description: "Тринитка на флісі. Розмір 62см."
@@ -114,7 +115,7 @@ export const goods = [
         id: "id-11",
         price: 570,
         photo: [
-            {src:"https://i.ibb.co/d7NXk3V/photo-2023-03-15-19-48-42-3.jpg", alt:"плед"}, 
+            {src:"https://i.ibb.co/ngf1zQj/photo-2023-04-09-12-28-12.jpg", alt:"плед"}, 
             {src:"https://i.ibb.co/PQYj1ss/photo-2023-03-15-19-48-42-2.jpg", alt:"в'язка зблизька"}
     ],
         category: ["текстиль","ковдри", "покривала та пледи"],
@@ -126,43 +127,43 @@ export const goods = [
         id: "id-12",
         price: 120,
         photo: [
-            {src:"https://i.ibb.co/9TLhYHF/photo-2023-03-15-19-48-41-3.jpg", alt:""}, 
+            {src:"https://i.ibb.co/RYzDrFx/photo-2023-04-09-12-41-10.jpg", alt:"подушка"}, 
     ],
         category: [],
         description: "Під замовлення"
     },
 
     {
-        name: "Куртка демісезонна бежева",
+        name: "Костюм тройка",
         id: "id-13",
         price: 650,
         photo: [
-            {src:"https://i.ibb.co/3r4b00h/photo-2023-03-15-19-48-41-2.jpg", alt:"Куртка демісезонна бежева"}, 
+            {src:"https://i.ibb.co/mhrWs2Z/photo-2023-04-09-12-34-17.jpg", alt:"Костюм тройка"}, 
     ],
         category: ["одяг"],
-        description: "Розміри 98, 104, 110, 116. Капюшон змінається"
+        description: "Під замовлення"
     },
 
     {
-        name: "Куртка демісезонна темносиня",
+        name: "Костюм тройка",
         id: "id-14",
         price: 650,
         photo: [
-            {src:"https://i.ibb.co/cDtR12T/photo-2023-03-15-19-48-41.jpg", alt:"Куртка демісезонна темносиня"}, 
+            {src:"https://i.ibb.co/bm0tZHy/photo-2023-04-09-12-34-26.jpg", alt:"Костюм тройка"}, 
     ],
         category: ["одяг"],
-        description: "Розміри 98, 104, 110, 116. Капюшон змінається"
+        description: "Під замовлення"
     },
 
     {
         name: "Комплект чоловічок + шапочка",
         id: "id-15",
-        price: 320,
+        price: 255,
         photo: [
-            {src:"https://i.ibb.co/C90Hwc2/photo-2023-03-15-19-48-42-4.jpg", alt:""}, 
+            {src:"https://i.ibb.co/HBtpbwJ/photo-2023-04-09-12-33-39.jpg", alt:""}, 
     ],
         category: [],
-        description: "Розмір 62"
+        description: "Розмір 56 (інтерлок)"
     },
 
     {
@@ -175,25 +176,135 @@ export const goods = [
     },
 
     {
-        name: "Пелюшки баєві",
+        name: "Костюм двонитка",
         id: "id-17",
-        price: 73,
+        price: 500,
         photo: [
-            {src:"https://i.ibb.co/MRN9Q1m/photo-2023-03-15-19-48-43-7.jpg", alt:"Пелюшки баєві"}, 
+            {src:"https://i.ibb.co/ssGF8b5/photo-2023-04-09-12-35-28.jpg", alt:"Костюм двонитка"}, 
     ],
-        category: ["текстиль"],
-        description: "Розмір 80/90"
+        category: ["одяг"],
+        description: "Розмір: 98, 104, 110, 116, 122. Під замовлення."
     },
 
     {
-        name: "Сенсорні кульки",
+        name: "Костюм двонитка",
         id: "id-18",
+        price: 500,
+        photo: [
+            {src:"https://i.ibb.co/rmNqRp9/photo-2023-04-09-12-35-16.jpg", alt:"Костюм двонитка"}
+    ],
+        category: ["одяг"],
+        description: "Розмір: 98, 104, 110, 116, 122. Під замовлення."
+    },
+
+    {
+        name: "Ортопедична подушка",
+        id: "id-19",
+        price: 120,
+        photo: [
+            {src:"https://i.ibb.co/D1K2rtm/photo-2023-04-09-12-41-23.jpg", alt:"подушка"}, 
+    ],
+        category: [],
+        description: "Під замовлення"
+    },
+
+    {
+        name: "Ортопедична подушка",
+        id: "id-20",
+        price: 120,
+        photo: [
+            {src:"https://i.ibb.co/BtgjvxH/photo-2023-04-09-12-41-32.jpg", alt:"подушка"}, 
+    ],
+        category: [],
+        description: "Під замовлення"
+    },
+
+    {
+        name: "Ортопедична подушка",
+        id: "id-21",
+        price: 120,
+        photo: [
+            {src:"https://i.ibb.co/BPQ24mW/photo-2023-04-09-12-41-39.jpg", alt:"подушка"}, 
+    ],
+        category: [],
+        description: "Під замовлення"
+    },
+
+    {
+        name: "Плюшевий плед",
+        id: "id-22",
+        price: 570,
+        photo: [
+            {src:"https://i.ibb.co/PQYj1ss/photo-2023-03-15-19-48-42-2.jpg", alt:"в'язка зблизька"}
+    ],
+        category: ["текстиль","ковдри", "покривала та пледи"],
+        description: "Дитячий плюшевий плед для немовлят Пуффі із неймовірно м'якої, ніжної гіпоалергенної пряжі. Виріб легкий, ніжний та гарний в експлуатації, не електризується, не линяє. Вашій дитинці під таким пледом буде тепло та затишно"
+    },
+
+    {
+        name: "Костюм тройка",
+        id: "id-23",
+        price: 650,
+        photo: [
+            {src:"https://i.ibb.co/ZH9B89g/photo-2023-04-09-12-34-35.jpg", alt:"Костюм тройка"}, 
+    ],
+        category: ["одяг"],
+        description: "Під замовлення"
+    },
+
+    {
+        name: "Костюм тройка",
+        id: "id-24",
+        price: 650,
+        photo: [
+            {src:"https://i.ibb.co/TL1q2jq/photo-2023-04-09-12-34-43.jpg", alt:"Костюм тройка"}, 
+    ],
+        category: ["одяг"],
+        description: "Під замовлення"
+    },
+
+    {
+        name: "Спортивний костюм",
+        id: "id-25",
+        price: 640,
+        photo: [
+            {src:"https://i.ibb.co/1Mpfhc2/photo-2023-04-09-12-25-58.jpg", alt:"Костюм тройка"}, 
+    ],
+        category: ["одяг"],
+        description: "Розміри 92,98,104,110,116. Можна взяти окремо кофту або штани."
+    },
+
+    {
+        name: "Паралонові матрасики для купання",
+        id: "id-26",
         price: 150,
         photo: [
-            // {src:"https://ibb.co/M9114hL", alt:"невалідна картинка"}, 
-            {src:"https://i.ibb.co/V3CCZN8/photo-2023-03-15-19-48-43-6.jpg", alt:"валідна картинка"}
+            {src:"https://i.ibb.co/Ttyj2kM/photo-2023-04-09-12-26-28.jpg", alt:"Костюм тройка"}, 
     ],
-        category: ["Іграшки"],
+        category: ["одяг"],
+        description: "Під замовлення"
+    },
+
+    {
+        name: "Дощовик на прогулянкову коляску",
+        id: "id-27",
+        price: 190,
+        photo: [
+            {src:"https://i.ibb.co/DMnFvs8/photo-2023-04-09-12-27-38.jpg", alt:"Костюм тройка"}, 
+    ],
+        category: ["одяг"],
         description: ""
     },
+
+    {
+        name: "Жилєт",
+        id: "id-28",
+        price: 650,
+        photo: [
+            {src:"https://i.ibb.co/QjtxnNy/photo-2023-04-09-12-28-57.jpg", alt:"Костюм тройка"}, 
+    ],
+        category: ["одяг"],
+        description: "Під замовлення. Розмір:  92, 98, 104, 110"
+    },
+
 ]
